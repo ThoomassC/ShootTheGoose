588100292edbe7de802fe426b038291bd836fcab
Thomas.C <thomas.caron@viacesi.fr>
2023-12-12 22:10:12 +0100

    wip : Shooting management

--- ShootTheGoose/src/lib/components/Animal.svelte
+++ ShootTheGoose/src/lib/components/Animal.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
-    export let baseNameForUrl :string;
+	export let baseNameForUrl: string;
+	export let damage: boolean;
+
+	let health = 1;
+
+	function handleHit() {
+		if (damage) {
+			health = 0;
+		}
+	}
 </script>
 
-<img src="/images/{baseNameForUrl}.png"/>
-
+{#if health !== 0}
+	<img src="/images/{baseNameForUrl}.png" on:click={handleHit} />
+{/if}
---
+++ ShootTheGoose/src/lib/components/weapon.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { onMount } from 'svelte';
+	import { createEventDispatcher } from 'svelte';
+
+	let isShooting = false;
+	const dispatch = createEventDispatcher();
+
+	onMount(() => {
+		// Gérer l'affichage du tir ici
+	});
+
+	function shoot() {
+		isShooting = true;
+		// Logique du tir, émettre un événement pour indiquer que l'Animal a été touché
+		dispatch('hit');
+		setTimeout(() => {
+			isShooting = false;
+		}, 1000);
+	}
+</script>
+
+<div>
+	<button class="bg-red-500 text-white text-center" on:click={shoot}>Tirer</button>
+	{#if isShooting}
+		<p class="text-red-500 font-bold">Vous avez tiré !</p>
+	{/if}
+</div>
