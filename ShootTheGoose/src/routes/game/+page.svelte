ff1e5283ba0d77e8083b976a5149e0c2fcbe8f93
camille <camille.melkebeek@viacesi.fr>
2023-12-13 15:34:09 +0100

    Fix bugouh

---
+++ ShootTheGoose/src/routes/game/+page.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+	import Animal from '$lib/components/Animal.svelte';
+	import { onDestroy } from 'svelte';
+
+	let isShooting = false;
+	let isShootingText = false;
+
+	let randomPosition = getRandomPosition();
+
+	let left = randomPosition.x + 'px';
+	let top = randomPosition.y + 'px';
+
+	const audio = new Audio('/sounds/oie.mp3');
+
+	function handleShoot() {
+		isShooting = true;
+		audio.play();
+		setTimeout(() => {
+			isShootingText = true;
+
+			setTimeout(() => {
+				isShootingText = false;
+				randomPosition = getRandomPosition();
+				updateButtonStyle();
+			}, 1000);
+		}, 1000);
+	}
+
+	function getRandomPosition() {
+		const positionX = Math.floor(Math.random() * (window.innerWidth - 100));
+		const positionY = Math.floor(Math.random() * (window.innerHeight - 100));
+
+		return { x: positionX, y: positionY };
+	}
+
+	function updateButtonStyle() {
+		left = randomPosition.x + 'px';
+		top = randomPosition.y + 'px';
+	}
+
+	function handleMouseMove(event: any) {
+		const isMouseOverAnimal = event.target && event.target.closest('.animal-container') !== null;
+
+		if (!isShooting && !isMouseOverAnimal) {
+			randomPosition = { x: event.clientX, y: event.clientY };
+			updateButtonStyle();
+		}
+	}
+
+	// Déplacer l'oie aléatoirement toutes les 2 secondes (ou tout autre intervalle souhaité)
+	const moveInterval = setInterval(() => {
+		randomPosition = getRandomPosition();
+		updateButtonStyle();
+	}, 2000);
+
+	// Nettoyer l'intervalle lorsque le composant est détruit
+	onDestroy(() => {
+		clearInterval(moveInterval);
+	});
+</script>
+
+</div>
+<div class="overflow-hidden">
+	<div class="flex items-center justify-center">
+        <h1 style="font-size: 90px; font-weight: bold;">Shoot The Goose</h1>
+    </div>
+	
+	<button
+		style="position: absolute;"
+		class="animal-container"
+		style:left
+		style:top
+		on:click={handleShoot}
+		on:mousemove={handleMouseMove}
+		disabled={isShooting}>
+		
+		<Animal baseNameForUrl="oie_vol" dammage={isShooting} />
+	</button>
+</div>
+
+    <a href="../">
+        <button
+            class="ml-4 align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-full max-w-[150px] h-10 max-h-[90px] rounded-lg text-xs bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+            type="button">
+            {"<- -  "} Quitter la partie
+        </button>
+    </a>
+
+<button
+	style="z-index: 1; position: absolute;"
+	class="animal-container"
+	style:left
+	style:top
+	on:click={handleShoot}
+	on:mousemove={handleMouseMove}
+	disabled={isShooting}
+>
+	<Animal baseNameForUrl="oie_vol" dammage={isShooting} />
+</button>
+
+<style>
+	.animal-container {
+		transition:
+		left 0.5s ease,
+		top 0.5s ease;
+	}
+</style>
