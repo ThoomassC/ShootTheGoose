d4eded3b0a668dd8f3d58fe4b78a63d6ab7a7f05
Thomas.C <thomas.caron@viacesi.fr>
2023-12-13 11:27:11 +0100

    feat : random position animal

--- ShootTheGoose/src/routes/game/+page.svelte
+++ ShootTheGoose/src/routes/game/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import Animal from '$lib/components/Animal.svelte';
-  
+
 	let isShooting = false;
 	let isShootingText = false;
 	let randomPosition = getRandomPosition();
+	let left = randomPosition.x + 'px';
+	let top = randomPosition.y + 'px';
 
 	function handleShoot() {
 		isShooting = true;
@@ -12,16 +14,42 @@
 			setTimeout(() => {
 				isShootingText = false;
 				randomPosition = getRandomPosition();
+				updateButtonStyle();
 			}, 1000);
 		}, 1000);
 	}
 
 	function getRandomPosition() {
-		const positionX = Math.floor(Math.random() * window.innerWidth);
-		const positionY = Math.floor(Math.random() * window.innerHeight);
+		const positionX = Math.floor(Math.random() * (window.innerWidth - 100));
+		const positionY = Math.floor(Math.random() * (window.innerHeight - 100));
 
 		return { x: positionX, y: positionY };
 	}
+
+	function updateButtonStyle() {
+		left = randomPosition.x + 'px';
+		top = randomPosition.y + 'px';
+	}
+
+	function handleMouseMove(event) {
+		const isMouseOverAnimal = event.target && event.target.closest('.animal-container') !== null;
+
+		if (!isShooting && !isMouseOverAnimal) {
+			randomPosition = { x: event.clientX, y: event.clientY };
+			updateButtonStyle();
+		}
+	}
+
+	// Déplacer l'oie aléatoirement toutes les 2 secondes (ou tout autre intervalle souhaité)
+	const moveInterval = setInterval(() => {
+		randomPosition = getRandomPosition();
+		updateButtonStyle();
+	}, 2000);
+
+	// Nettoyer l'intervalle lorsque le composant est détruit
+	onDestroy(() => {
+		clearInterval(moveInterval);
+	});
 </script>
 
 <div class="relative">
@@ -45,11 +73,15 @@
 		<p class="text-red-500">Vous avez tiré sur une oie !</p>
 	{/if}
 
-	<button
-		style="z-index: 1; position: absolute; left: {randomPosition.x}px; top: {randomPosition.y}px;" 
-		on:click={handleShoot}
-		disabled={isShooting}
-	>
+<button
+	style="z-index: 1; position: absolute;"
+	on:click={handleShoot}
+	on:mousemove={handleMouseMove}
+	disabled={isShooting}
+	style:left
+	style:top
+>
+	<div class="animal-container">
 		<Animal baseNameForUrl="oie_vol" dammage={isShooting} />
-	</button>
-</div>
+	</div>
+</button>
