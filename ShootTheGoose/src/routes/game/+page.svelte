9e64009506fdf38cb1e8241d8fbe37ce2e357d1d
Thomas.C <thomas.caron@viacesi.fr>
2023-12-13 15:42:14 +0100

    fix : title

--- ShootTheGoose/src/routes/game/+page.svelte
+++ ShootTheGoose/src/routes/game/+page.svelte
@@ -59,13 +59,6 @@
 	});
 </script>
 
-<div class="bg-ground-pattern">
-	<h1
-		class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black"
-	>
-		Shoot the Goose !
-	</h1>
-</div>
 <div class="overflow-hidden">
 	<div class="flex items-center justify-center">
 		<h1 style="font-size: 90px; font-weight: bold;">Shoot The Goose</h1>
