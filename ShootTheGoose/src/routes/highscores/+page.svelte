8e0c47e6bdde44c815ce0d82cf06e31d9c4dfb99
Samuel <81042672+SamuelCarrico@users.noreply.github.com>
2023-12-13 13:06:16 +0100

    Feature/db connection (#2)
    * feat: images
    
    * feat: db

---
+++ ShootTheGoose/src/routes/highscores/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { PageServerData } from '../../../.svelte-kit/types/src/routes/highscores/$types';
+	export let data: PageServerData;
+	console.log(data);
+</script>
+
+<h2>Scores</h2>
+<table>
+	<thead>
+		<tr>
+			<th>Nom</th>
+			<th>Score</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each data.body.highScores as scores}
+			<tr>
+				<td>{scores.name}</td>
+				<td>{scores.point}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
