c3a81b864f4966e575318dd3be85302cc7d9df44
camille <camille.melkebeek@viacesi.fr>
2023-12-13 13:49:03 +0100

    Ajout background

--- ShootTheGoose/src/routes/game/+page.svelte
+++ ShootTheGoose/src/routes/game/+page.svelte
@@ -24,7 +24,7 @@
 	}
 </script>
 
-<div class="relative">
+<div class="bg-ground-pattern">
 	<h1
 		class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black"
 	>
--- ShootTheGoose/tailwind.config.js
+++ ShootTheGoose/tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: { backgroundImage: {
+			'ground-pattern': "url('https://i.ibb.co/8K3YTWZ/bg1.png')",
+		  }
+		}
 	},
 	plugins: []
 };
