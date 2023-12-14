d9c8c867b444ca358ebd5df8e622c20107f6b955
Samuel <81042672+SamuelCarrico@users.noreply.github.com>
2023-12-14 09:14:13 +0100

    Feature/score (#5)
    * Feature/db connection (#2)
    
    * feat: images
    
    * feat: db
    
    * feat : random position animal
    
    * Ajout background
    
    * feat : sound animal when kill
    
    * fix : type + width image oie
    
    * feat: life component
    
    * feat: life and score updating with good variables
    
    ---------
    
    Co-authored-by: Thomas.C <thomas.caron@viacesi.fr>
    Co-authored-by: camille <camille.melkebeek@viacesi.fr>

---
+++ ShootTheGoose/src/lib/components/Score.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	const begin = 0;
+	export let actualScore: number = begin;
+	export let shot: number;
+	export let pointIncrement: number;
+
+	$: if (shot > 0) {
+		actualScore += pointIncrement;
+	}
+</script>
+
+<h1 class="text-2xl font-extrabold text-white">{actualScore}</h1>
