cb86e03bc81a62ab9ac5c65f96bf44b6cd19667c
Thomas Caron <thomas.caron@viacesi.fr>
2023-12-14 14:35:55 +0100

    fix rebase

--- ShootTheGoose/src/routes/game/+page.svelte
+++ ShootTheGoose/src/routes/game/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Animal from '$lib/components/Animal.svelte';
 	import Life from '$lib/components/Life.svelte';
-	import { afterUpdate, onMount } from 'svelte';
 
 	let canard = {
 		type: 'canard',
@@ -24,22 +23,23 @@
 	}, 2000);
 </script>
 
-<div class="overflow-hidden">
-	<div class="flex items-center justify-center">
-		<h1 style="font-size: 60px; font-weight: bold;">Shoot The Goose</h1>
 <div class="custom-cursor h-screen">
+	<div class="overflow-hidden">
+		<div class="flex items-center justify-center">
+			<h1 style="font-size: 60px; font-weight: bold;">Shoot The Goose</h1>
+		</div>
+		{#each animals as { type, score, src }}
+			<Animal {type} {score} {src} />
+		{/each}
 	</div>
-	{#each animals as { type, score, src }}
-		<Animal {type} {score} {src} />
-	{/each}
-</div>
-<a href="../">
-	<button
-		class="ml-4 align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-full max-w-[150px] h-10 max-h-[90px] rounded-lg text-xs bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
-		type="button"
-	>
-		{'<- -  '} Quitter la partie
-	</button>
-</a>
+	<a href="../">
+		<button
+			class="ml-4 align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-full max-w-[150px] h-10 max-h-[90px] rounded-lg text-xs bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+			type="button"
+		>
+			{'<- -  '} Quitter la partie
+		</button>
+	</a>
 
-<Life></Life>
+	<Life></Life>
+</div>
